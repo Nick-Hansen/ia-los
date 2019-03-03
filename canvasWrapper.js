8b59f2fec115da7c1d5132a56f4a37b4ed312840
Nick Hansen <nick.hansen@iaai.com>
2019-03-03 06:54:54 -0600

    creating files

---
+++ canvasWrapper.js
@@ -0,0 +1,23 @@
+var canvasWrapper = (function() {
+    'user strict';
+    
+    //private variables
+    var xyz = 1;
+
+    //private functions
+    
+    //public accessor
+    var canvasWrapper = {};
+
+    //public functions
+    canvasWrapper.init = function (options) {
+        //clear state and set variables
+    };
+
+    canvasWrapper.doSomething = function() {
+        //insert code
+    };
+    
+    //return public APIs
+    return canvasWrapper;
+})();
